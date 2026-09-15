fdac76c54e7328969bcfa53156134065a4bd147b
Jican78 <cristijr735@gmail.com>
2026-09-15 12:30:31 +0100

    Fin  Lire un nombre

--- src/src/devine_mon_nombre.rs
+++ src/src/devine_mon_nombre.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Devine mon nombre !\n");
+    println!("Saisissez votre proposition.");
+    let mut input  = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    //let input: u32 = input.trim().parse().unwrap(); //pourquoi on fait ca
+    println!("Votre nombre : {}", input.trim());
+
 }
+
+//crates.io
+//supp src
+//compil: cargo build
+//faire rustling
