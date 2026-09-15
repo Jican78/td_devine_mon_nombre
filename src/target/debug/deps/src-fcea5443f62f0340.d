c46de841aa397afb8fe2bfdf34c55a3515adef58
Jican78 <cristijr735@gmail.com>
2026-09-15 10:08:56 +0100

    Preparation cargo

---
+++ src/target/debug/deps/src-fcea5443f62f0340.d
@@ -0,0 +1,5 @@
+/home/jican/Documents/L3/td_devine_mon_nombre/src/target/debug/deps/src-fcea5443f62f0340.d: src/main.rs
+
+/home/jican/Documents/L3/td_devine_mon_nombre/src/target/debug/deps/libsrc-fcea5443f62f0340.rmeta: src/main.rs
+
+src/main.rs:
